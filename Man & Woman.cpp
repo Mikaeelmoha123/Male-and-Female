8c07ca0a0668c918e58c426a7580cd7f36de1c43
Mikaeel <95021467+Mikaeelmoha123@users.noreply.github.com>
2022-10-10 08:38:45 -0700

    Add files via upload

---
+++ Man & Woman.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\t   O\t     O\n";
+    cout << "\t___|___\t  ___|___\n";
+    cout << "\t   |\t    /_\\ \n";
+    cout << "\t  / \\\t    | |\n";
+    cout << "\t\t ";
+}
